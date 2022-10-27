9bdb1fd782ef51056d411c783476d7e779de962b
Fede-Arevalo <arevalo.feder@gmail.com>
2022-10-27 21:54:49 +0200

    Ejercicio 1 y 2

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+const PORT = 3000;
+
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Redbull22*",
+  database: "expressSQL",
+});
+
+db.connect();
+
+// CREAR BASE DE DATOS
+app.get("/createdb", (req, res) => {
+  let sql = "CREATE DATABASE expressSQL";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send("Database created...");
+  });
+});
+
+// CREAR TABLA PRODUCTS
+app.get("/createTableProducts", (req, res) => {
+  let sql =
+    "CREATE TABLE products(id int AUTO_INCREMENT, name_product VARCHAR(50), PRIMARY KEY(id))";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send("Products table created...");
+  });
+});
+
+// CREAR TABLA CATEGORIES
+app.get("/createTableCategories", (req, res) => {
+  let sql =
+    "CREATE TABLE categories(id int AUTO_INCREMENT, name_category VARCHAR(150), PRIMARY KEY(id))";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send("Categories table created...");
+  });
+});
+
+// CREAR TABLA CATEGORIES_PRODUCTS
+app.get("/createTableProductsCategories", (req, res) => {
+  let sql =
+    "CREATE TABLE products_categories(id INT AUTO_INCREMENT, PRIMARY KEY(id), product_id INT, FOREIGN KEY(product_id) REFERENCES products(id), category_id INT, FOREIGN KEY(category_id) REFERENCES categories(id))";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send("Products_Categories table created...");
+  });
+});
+
+// CREAR NUEVO PRODUCTO
+app.post("/createProduct", (req, res) => {
+  let sql = `INSERT INTO products (name_product) values ('${req.body.name_product}');`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send("Product created...");
+  });
+});
+
+// CREAR NUEVA CATEGORÍA
+app.post("/createCategory", (req, res) => {
+  let sql = `INSERT INTO categories (name_category) values ('${req.body.name_category}');`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send("Category created...");
+  });
+});
+
+
+
+app.listen(PORT, () => console.log(`Servidor levantado en el puerto ${PORT}`));
