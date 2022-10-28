7c57bc3622014e04db5af23b040066f471155298
Fede-Arevalo <arevalo.feder@gmail.com>
2022-10-28 02:37:58 +0200

    Ejercicio 4

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/createdb", (req, res) => {
 // CREAR TABLA PRODUCTS
 app.get("/createTableProducts", (req, res) => {
   let sql =
-    "CREATE TABLE products(id int AUTO_INCREMENT, name_product VARCHAR(50), PRIMARY KEY(id))";
+    "CREATE TABLE products(id int AUTO_INCREMENT, name_product VARCHAR(50), PRIMARY KEY(id),category_id INT, FOREIGN KEY(category_id) REFERENCES products(id))";
   db.query(sql, (err, result) => {
     if (err) throw err;
     res.send("Products table created...");
@@ -43,31 +43,31 @@ app.get("/createTableCategories", (req, res) => {
   });
 });
 
-// CREAR TABLA CATEGORIES_PRODUCTS
-app.get("/createTableProductsCategories", (req, res) => {
-  let sql =
-    "CREATE TABLE products_categories(id INT AUTO_INCREMENT, PRIMARY KEY(id), product_id INT, FOREIGN KEY(product_id) REFERENCES products(id), category_id INT, FOREIGN KEY(category_id) REFERENCES categories(id))";
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    res.send("Products_Categories table created...");
-  });
-});
-
 // CREAR NUEVO PRODUCTO
 app.post("/createProduct", (req, res) => {
-  let sql = `INSERT INTO products (name_product) values ('${req.body.name_product}');`;
+  let sql = `INSERT INTO products (name_product, category_id) values ('${req.body.name_product}', ${req.body.category_id});`;
   db.query(sql, (err, result) => {
     if (err) throw err;
     res.send("Product created...");
   });
 });
 
-// CREAR NUEVA CATEGORÍA
+// CREAR NUEVA CATEGORIA
 app.post("/createCategory", (req, res) => {
   let sql = `INSERT INTO categories (name_category) values ('${req.body.name_category}');`;
   db.query(sql, (err, result) => {
     if (err) throw err;
     res.send("Category created...");
+  });
+});
+
+// RELACIONAR CATEGORIA CON PRODUCTO
+app.post("/relateCategory", (req, res) => {
+  let sql = `INSERT INTO expresssql.products_categories (product_id, category_id) values (${req.body.product_id}, ${req.body.category_id});`;
+  console.log(sql);
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send("Category related...");
   });
 });
 
@@ -86,6 +86,78 @@ app.put("/categories/id/:id", (req, res) => {
   db.query(sql, (err, result) => {
     if (err) throw err;
     res.send("Category updated...");
+  });
+});
+
+// SELECCIONAR TODOS LOS PRODUCTOS
+app.get("/products", (req, res) => {
+  let sql = "SELECT * FROM products";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+// SELECCIONAR TODAS LAS CATEGORIAS
+app.get("/categories", (req, res) => {
+  let sql = "SELECT * FROM categories";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+// SELECCIONAR PRODUCTOS CON CATEGORIAS
+app.get("/productsWithCategories", (req, res) => {
+  let sql = `SELECT products.name_product AS "product", categories.name_category AS "category" FROM products LEFT JOIN categories ON category_id = categories.id`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+// SELECCIONAR PRODUCTOS POR ID
+app.get("/productsById/id/:id", (req, res) => {
+  let sql = `SELECT products.name_product AS "product" FROM products WHERE id = ${req.params.id}`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+// SELECCIONAR PRODUCTOS DESCENDIENTES POR ID (La idea sería que muestre los "más recientes")
+app.get("/productsIdDesc", (req, res) => {
+  let sql = `SELECT * FROM products ORDER BY id DESC;`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+// SELECCIONAR CATEGORIA POR ID
+app.get("/categoryById/id/:id", (req, res) => {
+  let sql = `SELECT * FROM categories WHERE id = ${req.params.id};`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+// BUSCAR PRODUCTO POR NOMBRE
+app.get("/productByName/name/:name", (req, res) => {
+  let sql = `SELECT * FROM products WHERE name_product LIKE "%${req.params.name}%";`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+// BORRAR PRODUCTO POR ID
+app.delete("/deleteProduct/id/:id", (req, res) => {
+  let sql = `DELETE FROM products WHERE id = ${req.params.id};`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send(result);
   });
 });
 
